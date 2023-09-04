11640df384aa4821f0f9ca0af21d6d1db9d086a2
Mathwebb <matheus.webber@hotmail.com>
2023-09-04 15:38:25 -0300

    Creating all the models to map the application to the database

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from contact.models import Contact, Category
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone',)
+    ordering = ('-id',)
+    # list_filter = ('created_date',)
+    search_fields = ('id', 'first_name', 'last_name',)
+    list_per_page = 10
+    list_max_show_all = 100
+    list_editable = ('first_name', 'last_name',)
+    list_display_links = ('id', 'phone',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    ordering = ('-id',)
+    search_fields = ('id', 'name',)
+    list_per_page = 10
+    list_max_show_all = 100
+
