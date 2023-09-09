6a25a4b92d07c9b5efce74684643b9caef903a08
Mathwebb <matheus.webber@hotmail.com>
2023-09-09 19:22:44 -0300

    Creating the site areas to allow the updating and exclusion of contats

--- contact/forms.py
+++ contact/forms.py
@@ -1,12 +1,26 @@
 from typing import Any
 from contact.models import Contact
+from django.core.exceptions import ValidationError
 
 from django import forms
 
 class ContactForm(forms.ModelForm):
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*',
+            }
+        )
+    )
+
     class Meta:
         model = Contact
-        fields = ('first_name', 'last_name', 'phone',)
+        fields = (
+            'first_name', 'last_name', 'phone',
+            'email', 'description', 'category',
+            'picture',
+        )
     
     def clean(self) -> dict[str, Any]:
+        cleaned_data = self.cleaned_data
         return super().clean()
--- contact/urls.py
+++ contact/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     
     # contact
     path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
     path('contact/<int:contact_id>/', views.contact, name='contact'),
 ]
